099dbfcac383a3c2328956d0a1c0036813d95e8e
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-11-30 20:40:02 +0900

    gemの追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,27 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require_tree
+//= require jquery
 //= require moment
 //= require fullcalendar
+
+$(function(){
+  $(document).on('turbolinks:load',function(){
+    if($('#calendar').length){
+      function eventCalendar(){
+        return $('#calendar').fullCalendar({});
+      };
+      function clearCalendar(){
+        $('#calendar').html('');
+      };
+      $(document).on('turbolinks:load',function(){
+        eventCalendar();
+      });
+      $(document).on('turbolinks:before-cache',clearCalendar);
+      $('#calendar').fullCalendar({
+        evnets: '/events.json'
+      });
+    }
+  });
+});
